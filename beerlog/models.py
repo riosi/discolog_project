cc2cf471aa53acc10a94e17634f8d597a2e797f9
riosi <riosingryd@gmail.com>
2022-04-26 17:36:24 -0300

    first commit, first day, databse

---
+++ beerlog/models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from statistics import mean
+from typing import Optional
+from sqlmodel import SQLModel, Field 
+from sqlmodel import select 
+from pydantic import validator
+
+class Beer(SQLModel, table=True):
+    # optional faz com que o próprio sql nos dê um id automático
+    id: Optional[int] = Field(primary_key=True, default=None) 
+    name: str
+    style: str
+    flavor: int
+    image: int
+    cost: int
+    rate: int = 0
+    date: datetime = Field(default_factory=datetime.now)
+
+    @validator("flavor", "image", "cost") # os 3 campos que recebem a validação
+    def validate_ratings(cls, v, field): # classe, valor & campo
+        if v < 1 or v > 10:
+            raise RuntimeError(f"{field.name} must be between 1 and 10")
+        return v
+    
+    @validator("rate", always=True)
+    def calculate_rate(cls, v, values):
+        rate = mean([values["flavor"], values["image"], values["cost"]])
+        return int(rate)
+brewdog = Beer(name="Brewdog", style="NEIPA", flavor=6, image=8, cost=8)
+    
